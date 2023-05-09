e541546a05b59795c4aca7b4852a1378c36b379c
Sean Steimer <ssteimer@adobe.com>
2023-05-08 18:00:24 -0700

    first pass at import

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -16,20 +16,98 @@ const transformBlocks = (main, document) => {
   const sectionBreak = document.createElement('p');
   sectionBreak.innerHTML = '---';
 
+  // sections
+  main.querySelectorAll('section').forEach((section) => {
+    if (section.querySelector(':scope > .blue-box')) {
+      const metdataCells = [
+        ['Section Metadata'],
+        ['Style', 'Highlight'],
+      ];
+      const sectionMetadataBlock = WebImporter.DOMUtils.createTable(metdataCells, document);
+      section.append(sectionMetadataBlock);
+    }
+
+    section.insertAdjacentElement('beforeend', sectionBreak.cloneNode(true));
+  });
+
+  // blog feed
+  const blog = main.querySelector('.blog');
+  if (blog) {
+    const blogCells = [
+      ['Blog Feed'],
+    ];
+    const blogBlock = WebImporter.DOMUtils.createTable(blogCells, document);
+    blog.replaceWith(blogBlock);
+  }
+
+  // news feed
+  const news = main.querySelector('.news');
+  if (news) {
+    const newsCells = [
+      ['News Feed'],
+    ];
+    const newsBlock = WebImporter.DOMUtils.createTable(newsCells, document);
+    news.replaceWith(newsBlock);
+  }
+
   // hero
   const hero = main.querySelector('.heroimage');
   if (hero) {
-    if (hero.classList.contains('cmp-hero-syle_pink-bg')) {
-      const metdataCells = [
-        ['Section Metadata'],
-        ['Style', 'Alternate Hero']
-      ];
-      const sectionMetadataBlock = WebImporter.DOMUtils.createTable(metdataCells, document);
-      hero.append(sectionMetadataBlock);
-    }
+    const heroImg = hero.querySelector('.el-hero-image');
+    if (heroImg) {
+      hero.prepend(heroImg);
 
-    hero.append(sectionBreak.cloneNode(true));
+      if (hero.classList.contains('cmp-hero-syle_pink-bg')) {
+        const metdataCells = [
+          ['Section Metadata'],
+          ['Style', 'Alternate Hero'],
+        ];
+        const sectionMetadataBlock = WebImporter.DOMUtils.createTable(metdataCells, document);
+        hero.append(sectionMetadataBlock);
+      }
+
+      hero.append(sectionBreak.cloneNode(true));
+    }
   }
+
+  main.querySelectorAll('.row-cols').forEach((cols) => {
+    let areEqual = true;
+    let teaserCount = 0;
+    const numCols = cols.children.length;
+    [...cols.children].forEach((col) => {
+      const teaser = col.querySelector(':scope > .teaser');
+      if (teaser) {
+        if (teaser.querySelector('h5') && teaser.querySelector('.desc-tile') && teaser.querySelector('.more')) {
+          teaserCount += 1;
+        }
+      }
+    });
+    const areCards = teaserCount === numCols;
+    if (areCards) {
+      const cardCells = [
+        ['Cards'],
+      ];
+      [...cols.children].forEach((col) => {
+        const cardData = [col.querySelector('img'), [col.querySelector('h5'), col.querySelector('.desc-tile'), col.querySelector('.more')]];
+        cardCells.push(cardData);
+      });
+      const cardsBlock = WebImporter.DOMUtils.createTable(cardCells, document);
+      cols.replaceWith(cardsBlock);
+    } else {
+      const columnCells = [
+        ['Columns'],
+      ];
+      [...cols.children].forEach((col) => {
+        if (col.children.length > 0) {
+          columnCells.push([...col.children]);
+        }
+      });
+      if (columnCells.length > 1) {
+        const colsBlock = WebImporter.DOMUtils.createTable(columnCells, document);
+        cols.replaceWith(colsBlock);
+      }
+    }
+  });
 };
 
 const createMetadata = (main, document) => {
