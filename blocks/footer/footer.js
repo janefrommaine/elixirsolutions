3f19b59796879ac504ec2ee36769eee91faf5dcd
Ewing (Yu-An Lin) <chockerlin@gmail.com>
2023-05-10 09:27:31 -0700

    Footer
    * add footer basics
    
    * fix linting
    
    * fix social links
    
    * remove unused social css
    
    * move social icon image to footer folder
    
    * minor tweaks
    
    ---------
    
    Co-authored-by: Sean Steimer <ssteimer@adobe.com>

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,5 +1,19 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
-import { wrapImgsInLinks } from '../../scripts/scripts.js';
+import { wrapImgsInLinks, decorateLinks } from '../../scripts/scripts.js';
+
+const handleSocialLinks = (block) => {
+  const footerSocialContainer = block.querySelector('.footer-social ul');
+  footerSocialContainer.classList.add('footer-social-container');
+  Object.values(footerSocialContainer.children).forEach((child) => {
+    const link = child.querySelector('a');
+    const host = (new URL(link.href).hostname).split('.');
+    const social = host[0] === 'www' ? host[1] : host[0];
+    link.classList.add('footer-social-icon');
+    link.innerText = '';
+    link.classList.add(`footer-social-icon-${social}`);
+    child.replaceWith(link);
+  });
+};
 
 /**
  * loads and decorates the footer
@@ -20,7 +34,52 @@ export default async function decorate(block) {
     const footer = document.createElement('div');
     footer.innerHTML = html;
 
+    // add classes to footer sections
+    const sectionNames = ['image', 'links', 'social', 'legal', 'copyright'];
+    sectionNames.forEach((c, i) => {
+      const section = footer.children[i];
+      if (section) section.classList.add(`footer-${c}`);
+    });
+
+    // structure upper footer
+    const upperFooterNames = ['image'];
+    const upperFooter = document.createElement('div');
+    upperFooter.classList.add('footer-upper');
+    const upperFooterContent = document.createElement('div');
+    upperFooterContent.classList.add('footer-upper-content');
+    upperFooterNames.forEach((c) => {
+      const section = footer.querySelector(`.footer-${c}`);
+      if (section) upperFooterContent.appendChild(section);
+    });
+    upperFooter.appendChild(upperFooterContent);
+    footer.appendChild(upperFooter);
+
+    // structure link container
+    const linksNames = ['links', 'social'];
+    const linksContainer = document.createElement('div');
+    linksContainer.classList.add('footer-links-container');
+    linksNames.forEach((c) => {
+      const section = footer.querySelector(`.footer-${c}`);
+      if (section) linksContainer.appendChild(section);
+    });
+    upperFooterContent.appendChild(linksContainer);
+    handleSocialLinks(linksContainer);
+
+    // structure lower footer
+    const lowerFooterNames = ['legal', 'copyright'];
+    const lowerFooter = document.createElement('div');
+    lowerFooter.classList.add('footer-lower');
+    const lowerFooterContent = document.createElement('div');
+    lowerFooterContent.classList.add('footer-lower-content');
+    lowerFooterNames.forEach((c) => {
+      const section = footer.querySelector(`.footer-${c}`);
+      if (section) lowerFooterContent.appendChild(section);
+    });
+    lowerFooter.appendChild(lowerFooterContent);
+    footer.appendChild(lowerFooter);
+
     decorateIcons(footer);
+    decorateLinks(footer);
     wrapImgsInLinks(footer);
     block.append(footer);
   }
