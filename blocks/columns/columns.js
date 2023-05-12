afe705dba66db609adc0df0aa39bbb5af0e6516a
Yu-An Lin <yualin@adobe.com>
2023-05-12 16:48:13 -0700

    basics of teaser

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -3,6 +3,13 @@ export default function decorate(block) {
   block.classList.add(`columns-${cols.length}-cols`);
   const imgColClass = 'columns-img-col';
 
+  const isTeaser = block.classList.contains('teaser');
+
+  // teaser is also a variation of thirds
+  if (isTeaser) {
+    block.classList.add('thirds');
+  }
+
   // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
@@ -13,6 +20,18 @@ export default function decorate(block) {
         if (picWrapper && picWrapper.children.length <= 2) {
           // picture is only content in column
           picWrapper.classList.add(imgColClass);
+        }
+
+        if (isTeaser) {
+          const teaserWrapper = document.createElement('div');
+          teaserWrapper.classList.add('teaser-wrapper');
+          ['style-a', 'style-b', 'style-c'].forEach((style) => {
+            const bubble = document.createElement('i');
+            bubble.classList.add('bubble', style);
+            teaserWrapper.appendChild(bubble);
+          });
+          teaserWrapper.appendChild(pic);
+          col.appendChild(teaserWrapper);
         }
       }
     });
