4849e4203a4226d913e8ad8d9dd2e9250ec4a3c4
Sean Steimer <ssteimer@adobe.com>
2023-05-15 13:25:00 -0700

    stuff

--- blocks/formstack/formstack.js
+++ blocks/formstack/formstack.js
@@ -2,6 +2,7 @@ import { readBlockConfig } from '../../scripts/lib-franklin.js';
 
 function nodeScriptClone(node) {
   const script = document.createElement('script');
+  script.setAttribute('replaced', true);
   script.text = node.innerHTML;
 
   [...node.attributes].forEach((attr) => {
@@ -12,34 +13,77 @@ function nodeScriptClone(node) {
 }
 
 function nodeScriptReplace(node) {
-  if (node.tagName === 'SCRIPT') {
+  if (node.tagName === 'SCRIPT' && !node.hasAttribute('replaced')) {
     node.parentNode.replaceChild(nodeScriptClone(node), node);
-  } else {
-    [...node.childNodes].forEach((child) => {
-      nodeScriptReplace(child);
-    });
+    return true;
   }
 
-  return node;
+  for (let i = 0; i < node.childNodes.length; i += 1) {
+    const child = node.childNodes[i];
+    const replaced = nodeScriptReplace(child);
+    if (replaced) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
-export default function decorate(block) {
-  const docWrite = document.write;
-  let innerHtml = '';
-  document.write = (s) => {
-    innerHtml += s;
-  };
-  const cfg = readBlockConfig(block);
-  const scriptSrc = `https://elixir-form.formstack.com/forms/js.php/${cfg['form-id']}?no_style_strict=1`;
-  const script = document.createElement('script');
-  script.src = scriptSrc;
-  script.setAttribute('type', 'text/javascript');
-  block.innerHTML = '';
-  block.append(script);
+class FormstackForm extends HTMLElement {
+  constructor() {
+    super();
 
-  setTimeout(() => {
-    block.innerHTML = innerHtml;
-    document.write = docWrite;
-    nodeScriptReplace(block);
-  }, 5000);
+    // overwrite doc write to append to the shadow dom
+    const shadow = this.attachShadow({ mode: 'open' });
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('form-wrapper');
+    shadow.appendChild(wrapper);
+
+    let formHtml = '';
+    const docWrite = document.write;
+    document.write = (s) => {
+      formHtml += s;
+    };
+
+    let htmlLen = 0;
+    let prevHtmlLen = -1;
+    const interValId = setInterval(() => {
+      htmlLen = formHtml.length;
+      if (htmlLen > prevHtmlLen) {
+        // keep going
+        prevHtmlLen = htmlLen;
+      } else {
+        // write
+        clearInterval(interValId);
+        document.write = docWrite;
+        wrapper.innerHTML = formHtml;
+
+        const scriptIntervalId = setInterval(() => {
+          const replaced = nodeScriptReplace(wrapper);
+          if (!replaced) {
+            clearInterval(scriptIntervalId);
+          }
+        }, 300);
+      }
+    }, 250);
+    this.renderForm();
+  }
+
+  renderForm() {
+    const shadow = this.shadowRoot;
+    const formId = this.getAttribute('formId');
+    if (formId) {
+      const scriptSrc = `https://elixir-form.formstack.com/forms/js.php/${formId}?no_style_strict=1`;
+      const script = document.createElement('script');
+      script.src = scriptSrc;
+      script.setAttribute('type', 'text/javascript');
+      shadow.appendChild(script);
+    }
+  }
+}
+customElements.define('formstack-form', FormstackForm);
+
+export default function decorate(block) {
+  const cfg = readBlockConfig(block);
+  block.innerHTML = `<formstack-form formId=${cfg['form-id']}></formstack-form>`;
 }
