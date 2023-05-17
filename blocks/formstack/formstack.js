0f08a0fb02fb5a504577baa51c873a624cbb16ed
Sean Steimer <ssteimer@adobe.com>
2023-05-17 08:39:50 -0700

    refactoring formstack

--- blocks/formstack/formstack.js
+++ blocks/formstack/formstack.js
@@ -1,5 +1,44 @@
 import { readBlockConfig } from '../../scripts/lib-franklin.js';
 
+function loadFormScripts(scriptsToLoad, block) {
+  // delay here between each script allows each script to load and parse before we load the next one
+  const scriptLoadInterval = setInterval(() => {
+    if (scriptsToLoad.length === 0) {
+      clearInterval(scriptLoadInterval);
+      // once all script are loaded call loadFormstack
+      if (typeof window.loadFormstack === 'function') {
+        window.loadFormstack();
+      }
+      return;
+    }
+    const toLoad = scriptsToLoad.shift();
+    const scr = document.createElement('script');
+    if (toLoad.src) scr.src = toLoad.src;
+    if (toLoad.type) scr.type = toLoad.type;
+    if (toLoad.textContent) {
+      scr.innerHTML = toLoad.textContent;
+    }
+    block.appendChild(scr);
+  }, 500);
+}
+
+function loadForm(formHtml, wrapper, block) {
+  const dp = new window.DOMParser();
+  const html = dp.parseFromString(formHtml, 'text/html');
+  const embed = html.querySelector('.fsEmbed');
+  let scriptsToLoad;
+  if (embed) {
+    wrapper.append(embed);
+    // script elements aren't actually processed, so need to remove and re-add
+    scriptsToLoad = [...embed.querySelectorAll('script')].map((s) => {
+      const { src, type, textContent } = s;
+      s.remove();
+      return { src, type, textContent };
+    });
+    loadFormScripts(scriptsToLoad, block);
+  }
+}
+
 export default function decorate(block) {
   const cfg = readBlockConfig(block);
   const formId = cfg['form-id'];
@@ -23,36 +62,6 @@ export default function decorate(block) {
 
   setTimeout(() => {
     document.write = docWrite;
-    const dp = new window.DOMParser();
-    const html = dp.parseFromString(formHtml, 'text/html');
-    const embed = html.querySelector('.fsEmbed');
-    let scriptsToLoad;
-    if (embed) {
-      scriptsToLoad = [...embed.querySelectorAll('script')].map((s) => {
-        const { src, type, textContent } = s;
-        s.remove();
-        return { src, type, textContent };
-      });
-      wrapper.append(embed);
-      const scriptLoadInterval = setInterval(() => {
-        if (scriptsToLoad.length === 0) {
-          clearInterval(scriptLoadInterval);
-          if (typeof window.loadFormstack === 'function') {
-            window.loadFormstack();
-          }
-          return;
-        }
-        const toLoad = scriptsToLoad.shift();
-        const scr = document.createElement('script');
-        if (toLoad.src) scr.src = toLoad.src;
-        if (toLoad.type) scr.type = toLoad.type;
-        if (toLoad.textContent) {
-          scr.innerHTML = toLoad.textContent;
-        }
-        block.appendChild(scr);
-      }, 500);
-    }
+    loadForm(formHtml, wrapper, block);
   }, 3000);
-
-  // block.innerHTML = `<formstack-form formId=${formId}></formstack-form>`;
 }
