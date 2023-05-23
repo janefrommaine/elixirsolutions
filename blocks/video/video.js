70e3fbf717c27146bffdadda8390d8f8bccd2a1f
Sean Steimer <ssteimer@adobe.com>
2023-05-22 20:57:23 -0700

    lazy video load w/ placeholder

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -8,11 +8,27 @@ export default async function decorate(block) {
   const a = block.querySelector('a');
   if (a) {
     const source = a.href;
-    const img = block.querySelector('img');
-    block.innerHTML = `
-    <video controls poster="${img ? img.src : ''}">
-      <source src="${source}" type="video/${source.split('.').pop()}" >
-    </video>
-    `;
+    const pic = block.querySelector('picture');
+    if (pic) {
+      const wrapper = document.createElement('div');
+      wrapper.className = 'video-placeholder';
+      wrapper.innerHTML = '<div class="video-placeholder-play"><button title="Play"></button></div>';
+      wrapper.prepend(pic);
+      wrapper.addEventListener('click', () => {
+        block.innerHTML = `
+        <video controls autoplay>
+          <source src="${source}" type="video/${source.split('.').pop()}" >
+        </video>
+        `;
+      });
+      block.innerHTML = '';
+      block.append(wrapper);
+    } else {
+      block.innerHTML = `
+      <video controls>
+        <source src="${source}" type="video/${source.split('.').pop()}" >
+      </video>
+      `;
+    }
   }
 }
