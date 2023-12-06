392ecb2c452b406d884cca0972a59c42f916681f
Jane Connolly <jconnolly@elixirsolutions.com>
2023-12-06 11:48:24 -0500

    145 - Sidebar add ability to add background color using background metadata, remove max-width on images and videos for desktop breakpoint

--- blocks/sidebar/sidebar.js
+++ blocks/sidebar/sidebar.js
@@ -1,1 +1,12 @@
-export default function decorate() {}
+/**
+ * decorates the sidebar
+ * @param {Element} block The header block element
+ */
+export default function decorate(block) {
+  const sidebarWrapper = block.parentElement;
+
+  if (sidebarWrapper.dataset && sidebarWrapper.dataset.background) {
+    const bgClass = `bg-${sidebarWrapper.dataset.background.replace(' ', '-').toLowerCase()}`;
+    sidebarWrapper.classList.add(bgClass);
+  }
+}
